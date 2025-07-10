4af4f1adda4365694527b17489e4e034d0852d01
Henry Jr Zai <hjuniorzai@gmail.com>
2025-07-10 13:37:51 +0700

    fix : memperbaiki route untuk admin dengan menambahkan prefix

--- routes/web.php
+++ routes/web.php
@@ -8,13 +8,10 @@ use App\Http\Controllers\TransaksiController;
 use App\Models\Katalog;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 
-Route::get('/dashboard', [\App\Http\Controllers\DashboardController::class, 'index'])
-    ->middleware(['auth', 'adminManajer', 'verified'])
-    ->name('dashboard');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
@@ -22,12 +19,20 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-Route::resource('produk', ProdukController::class)->middleware(['auth', 'adminManajer']);
-Route::resource('katalog', KatalogController::class)->middleware(['auth', 'adminManajer']);
-Route::resource('transaksi', TransaksiController::class)->middleware(['auth', 'adminManajer']);
-Route::resource('pengiriman', PengirimanController::class)->middleware(['auth', 'adminManajer']);
-Route::resource('admin', \App\Http\Controllers\AdminController::class)->middleware(['auth', 'adminManajer']);
-Route::resource('manajer', \App\Http\Controllers\ManajerController::class)->middleware(['auth', 'adminManajer']);
-Route::resource('pelanggan', \App\Http\Controllers\PelangganController::class)->middleware(['auth', 'adminManajer']);
+Route::prefix('admin')->middleware(['auth', 'adminManajer'])->group(function () {
+    Route::get('/dashboard', [\App\Http\Controllers\DashboardController::class, 'index'])
+        ->middleware(['auth', 'adminManajer', 'verified'])
+        ->name('dashboard');
+
+    Route::resource('produk', ProdukController::class)->middleware(['auth', 'adminManajer']);
+    Route::resource('katalog', KatalogController::class)->middleware(['auth', 'adminManajer']);
+    Route::resource('transaksi', TransaksiController::class)->middleware(['auth', 'adminManajer']);
+    Route::resource('pengiriman', PengirimanController::class)->middleware(['auth', 'adminManajer']);
+    Route::resource('admin', \App\Http\Controllers\AdminController::class)->middleware(['auth', 'adminManajer']);
+    Route::resource('manajer', \App\Http\Controllers\ManajerController::class)->middleware(['auth', 'adminManajer']);
+    Route::resource('pelanggan', \App\Http\Controllers\PelangganController::class)->middleware(['auth', 'adminManajer']);
+});
+
 
 require __DIR__.'/auth.php';
+
